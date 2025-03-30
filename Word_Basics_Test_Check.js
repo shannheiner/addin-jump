7c45dcc495a86de596c7275ddea326ab655b5761
shannheiner <71232549+shannheiner@users.noreply.github.com>
2025-03-29 21:26:08 -0600

    Add files via upload

--- Word_Basics_Test_Check.js
+++ Word_Basics_Test_Check.js
@@ -26,7 +26,9 @@
                     { text: "LineSpacing2", type: "paragraph", property: "lineSpacing", expected: 18 },
                     { text: "Align_Left", type: "paragraph", property: "alignment", expected: "Left" },
 
-                    { text: "LineSpacing4", type: "paragraph", property: "Word.Alignment", expected: Centered},
+                    { text: "LineSpacing5", type: "paragraph", property: "Word.Alignment", expected: 18},
+
+                    { text: "LineSpacing7", type: "paragraph", property: "Word.Alignment", expected: "Centered"},
 
                     { text: "LineSpacing3", type: "paragraph", property: "alignCenter", expected: 18 },
                     { text: "LineSpacing4", type: "paragraph", property: "lineSpacing", expected: 18 },
